b2d6992f3057710bc2e832ba18e6aded4f05c878
Stef Buurman <St3fbuurman@gmail.com>
2024-03-05 11:19:50 +0100

    testCommit

--- BioscoopReserveringsapplicatie/Program.cs
+++ BioscoopReserveringsapplicatie/Program.cs
@@ -5,5 +5,9 @@ public class Program
     public static void Main(string[] args)
     {
         AnsiConsole.Markup("[underline red]Hello[/] World!");
+        var calendar = new Calendar(2020, 10);
+        calendar.AddCalendarEvent(2020, 10, 11);
+        calendar.HighlightStyle(Style.Parse("yellow bold"));
+        AnsiConsole.Write(calendar);
     }
 }
