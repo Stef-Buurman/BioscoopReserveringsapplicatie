e3178f3c74b7be35ddfcfabc6743eb21faedc96c
Pieter Smit <1058663@hr.nl>
2024-03-20 10:52:50 +0100

    Removed methods, looped no account found

--- BioscoopReserveringsapplicatie/Presentation/UserLogin.cs
+++ BioscoopReserveringsapplicatie/Presentation/UserLogin.cs
@@ -15,8 +15,8 @@ static class UserLogin
         string email = Console.ReadLine();
         Console.Write("Please enter your password: ");
         string password = Console.ReadLine();
-        accountsLogic.Login(email, password);
-        if (AccountsLogic.CurrentAccount != null)
+        AccountModel acc = accountsLogic.CheckLogin(email, password);
+        if (acc != null)
         {
             ColorConsole.WriteColorLine($"[Welcome back {acc.FullName}]", ConsoleColor.Green);
             Console.WriteLine($"Your email is {acc.EmailAddress}");
