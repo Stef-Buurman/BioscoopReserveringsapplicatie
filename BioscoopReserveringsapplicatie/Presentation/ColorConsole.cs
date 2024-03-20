d6bee188e1a8bfb7374b4d0d59d62a188c66ca11
Stef Buurman <St3fbuurman@gmail.com>
2024-03-20 10:20:36 +0100

    Added ColorConsole class and Selection menu

---
+++ BioscoopReserveringsapplicatie/Presentation/ColorConsole.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+public static class ColorConsole 
+{
+    public static void WriteColorLine(string message, ConsoleColor color)
+    {
+        WriteColor(message, color);
+        Console.WriteLine();
+    }
+    public static void WriteColor(string message, ConsoleColor color)
+    {
+        var piecesOfMessage = Regex.Split(message, @"(\[[^\]]*\])");
+
+        for (int i = 0; i < piecesOfMessage.Length; i++)
+        {
+            string messagePeace = piecesOfMessage[i];
+
+            if (messagePeace.StartsWith("[") && messagePeace.EndsWith("]"))
+            {
+                Console.ForegroundColor = color;
+                messagePeace = messagePeace.Substring(1, messagePeace.Length - 2);
+            }
+
+            Console.Write(messagePeace);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ BioscoopReserveringsapplicatie/Presentation/SelectionMenu.cs
@@ -0,0 +1,72 @@
+﻿static class SelectionMenu
+{
+    public static T Create<T>(List<Option<T>> options)
+    {
+        Console.CursorVisible = false;
+        int index = 0;
+
+        WriteMenu(options, options[index]);
+
+        ConsoleKeyInfo keyinfo;
+        do
+        {
+            keyinfo = Console.ReadKey();
+            if (keyinfo.Key == ConsoleKey.DownArrow)
+            {
+                if (index + 1 < options.Count)
+                {
+                    index++;
+                    WriteMenu(options, options[index]);
+                }
+            }
+            if (keyinfo.Key == ConsoleKey.UpArrow)
+            {
+                if (index - 1 >= 0)
+                {
+                    index--;
+                    WriteMenu(options, options[index]);
+                }
+            }
+            if (keyinfo.Key == ConsoleKey.Enter)
+            {
+                options[index].Select();
+                Console.CursorVisible = true;
+                return options[index].Value;
+            }
+        }
+        while (keyinfo.Key != ConsoleKey.X) ;
+
+        Console.Clear();
+        Console.CursorVisible = true;
+        return default;
+    }
+
+    public static T Create<T>(List<T> options)
+    {
+        List<Option<T>> optionList = new List<Option<T>>();
+        foreach (T option in options)
+        {
+            optionList.Add(new Option<T>(option));
+        }
+        return Create(optionList);
+    }
+
+    static void WriteMenu<T>(List<Option<T>> options, Option<T> selectedOption)
+    {
+        Console.Clear();
+
+        foreach (Option<T> option in options)
+        {
+            if (option == selectedOption)
+            {
+                Console.Write("> ");
+            }
+            else
+            {
+                Console.Write(" ");
+            }
+
+            Console.WriteLine(option.Name);
+        }
+    }
+}
