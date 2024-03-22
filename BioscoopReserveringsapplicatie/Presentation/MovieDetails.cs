a567accaf65cad5b9816e2fb8637de28165b8b7a
Stef Buurman <St3fbuurman@gmail.com>
2024-03-22 11:23:13 +0100

    Added commands, fixed a bug and added ActionBeforeMenu

--- BioscoopReserveringsapplicatie/Presentation/MovieDetails.cs
+++ BioscoopReserveringsapplicatie/Presentation/MovieDetails.cs
@@ -1,10 +1,11 @@
 static class MovieDetails
 {
     static private MoviesLogic MoviesLogic = new MoviesLogic();
+    private static MovieModel movie;
 
     public static void Start(int movieId)
     {
-        MovieModel movie = MoviesLogic.GetMovieById(movieId);
+        movie = MoviesLogic.GetMovieById(movieId);
 
         var options = new List<Option<string>>
             {
@@ -12,6 +13,11 @@ static class MovieDetails
                 new Option<string>("Delete movie", () => MovieDelete.Start(movieId)),
                 new Option<string>("Back", () => {Console.Clear(); MovieOverview.Start();}),
             };
-        SelectionMenu.Create(options, $"Movie details:\nTitle: {movie.Title} \nDescription: {movie.Description} \nGenre: {movie.Genre} \nRating: {movie.Rating} \n\nWhat would you like to do?");
+        SelectionMenu.Create(options, Print);
+    }
+
+    private static void Print()
+    {
+        if (movie != null) Console.WriteLine($"Movie details:\nTitle: {movie.Title} \nDescription: {movie.Description} \nGenre: {movie.Genre} \nRating: {movie.Rating} \n\nWhat would you like to do?");
     }
 }
