e2958fa95836e2e97d44d65b139364d6a0d26833
Pieter Smit <1058663@hr.nl>
2024-03-25 15:57:11 +0100

    Alle Console.WriteLines vertaald van Engels naar Nederlands

--- BioscoopReserveringsapplicatie/Presentation/MovieOverview.cs
+++ BioscoopReserveringsapplicatie/Presentation/MovieOverview.cs
@@ -24,7 +24,7 @@ namespace BioscoopReserveringsapplicatie
                 options.Add(new Option<int>(movie.Id, movie.Title));
             }
 
-        options.Add(new Option<int>(0, "Back", () => { Console.Clear(); AdminMenu.Start(); }));
+        options.Add(new Option<int>(0, "Terug", () => { Console.Clear(); AdminMenu.Start(); }));
 
             int movieId = SelectionMenu.Create(options, Print);
             Console.Clear();
