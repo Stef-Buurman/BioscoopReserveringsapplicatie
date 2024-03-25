9a60e4e4e32458999b0eb5c0ba2e87bc50983df5
Stef Buurman <St3fbuurman@gmail.com>
2024-03-25 16:01:55 +0100

    Added Experience tests without intensity

---
+++ BioscoopReserveringsapplicatieTests/ExperienceTest.cs
@@ -0,0 +1,44 @@
+﻿using BioscoopReserveringsapplicatie;
+
+namespace BioscoopReserveringsapplicatieTests
+{
+    [TestClass]
+    public class ExperienceTest
+    {
+        private ExperiencesLogic _experiencesLogic = new ExperiencesLogic();
+
+        [TestMethod]
+        public void IncorrectExperienceNull()
+        {
+            Assert.IsFalse(_experiencesLogic.ValidateExperience(null));
+        }
+
+        [TestMethod]
+        public void IncorrectExperienceName()
+        {
+            ExperiencesModel experience = new ExperiencesModel("", 0, 0, 0);
+            Assert.IsFalse(_experiencesLogic.ValidateExperience(experience));
+            ExperiencesModel experience2 = new ExperiencesModel(null, 0, 0, 0);
+            Assert.IsFalse(_experiencesLogic.ValidateExperience(experience2));
+        }
+
+        //[TestMethod]
+        //public void IncorrectExperienceIntensity()
+        //{
+        //}
+
+        [TestMethod]
+        public void IncorrectExperienceTimeLength()
+        {
+            ExperiencesModel experience = new ExperiencesModel("test1", 0, 0, -10);
+            Assert.IsFalse(_experiencesLogic.ValidateExperience(experience));
+        }
+
+        [TestMethod]
+        public void CorrectExperience()
+        {
+            ExperiencesModel experience = new ExperiencesModel("test1", 0, 0, 10);
+            Assert.IsTrue(_experiencesLogic.ValidateExperience(experience));
+        }
+    }
+}
