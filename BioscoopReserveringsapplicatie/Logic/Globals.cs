159c4fce01df2c49b40500536c21985d98c27aad
Stef Buurman <St3fbuurman@gmail.com>
2024-03-25 15:36:18 +0100

    Created new Develop directory

--- BioscoopReserveringsapplicatie/Logic/Globals.cs
+++ BioscoopReserveringsapplicatie/Logic/Globals.cs
@@ -1,10 +1,9 @@
 ﻿public static class Globals
 {
-    static string CurrentDirectoryDevelop = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+    static string CurrentDirectoryDevelop = System.IO.Path.GetFullPath(System.IO.Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, @"BioscoopReserveringsapplicatie"));
     static string CurrentDirectoryProduction = Environment.CurrentDirectory;
     public static string currentDirectory = CurrentDirectoryProduction;
 
     public static readonly ConsoleColor TitleColor = ConsoleColor.Cyan;
     public static readonly ConsoleColor ColorInputcClarification = ConsoleColor.Blue;
 }
-
