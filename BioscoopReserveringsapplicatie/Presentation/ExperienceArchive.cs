c659f20039be997a767ce1f922d5a71aed749a66
Pieter Smit <1058663@hr.nl>
2024-04-02 14:47:43 +0200

    added archived bool to experiences.json, added ArchiveExperience method, added ExperienceArchive class

---
+++ BioscoopReserveringsapplicatie/Presentation/ExperienceArchive.cs
@@ -0,0 +1,13 @@
+namespace BioscoopReserveringsapplicatie
+{
+    static class ExperienceArchive
+    {
+        static private ExperiencesLogic ExperienceLogic = new ExperiencesLogic();
+
+        public static void Start(int experienceId)
+        {
+            
+        }
+
+    }
+}
