5ed86f50013e3f7329029010b1181ec3d9439eca
Stef Buurman <St3fbuurman@gmail.com>
2024-04-02 15:14:08 +0200

    created GenreListConverter when reading json

---
+++ BioscoopReserveringsapplicatie/DataModels/JsonConvert/GenreListConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BioscoopReserveringsapplicatie
+{
+    public class GenreListConverter : JsonConverter<List<Genre>>
+    {
+        public override List<Genre> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            List<string> genres = JsonSerializer.Deserialize<List<string>>(ref reader, options);
+            if (genres == null) return null;
+
+            List<Genre> result = new List<Genre>();
+            foreach (var genre in genres)
+            {
+                if (Enum.TryParse(genre, out Genre genreEnum)) result.Add(genreEnum);
+            }
+
+            return result;
+        }
+
+        public override void Write(Utf8JsonWriter writer, List<Genre> value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
