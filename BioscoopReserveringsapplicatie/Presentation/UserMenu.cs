aed0f3983f7e371195b316016952b908982d7d7a
NickWijmans <nickwijmans@gmail.com>
2024-04-02 15:48:22 +0200

    Bug fix for logout and currentUser not being cleared

--- BioscoopReserveringsapplicatie/Presentation/UserMenu.cs
+++ BioscoopReserveringsapplicatie/Presentation/UserMenu.cs
@@ -12,7 +12,7 @@ namespace BioscoopReserveringsapplicatie
                 {
                     new Option<string>("Experienceoverzicht", () => PreferredExperiences.Start()),
                     new Option<string>("Mijn account", () => Profile.Start()),
-                    new Option<string>("Uitloggen", () => LandingPage.Start()),
+                    new Option<string>("Uitloggen", () => UserLogic.LogOut()),
                 };
                 SelectionMenu.Create(options, () => Console.WriteLine($"Welkom {CurrentUser.FullName}!\n"));
             }
