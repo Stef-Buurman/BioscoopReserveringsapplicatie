571d6f86fd589e898654accc6b5ca9d6cbab5f6c
NickWijmans <nickwijmans@gmail.com>
2024-04-02 14:45:31 +0200

    Added experience overview + detail

--- BioscoopReserveringsapplicatie/Presentation/AdminMenu.cs
+++ BioscoopReserveringsapplicatie/Presentation/AdminMenu.cs
@@ -13,7 +13,7 @@ namespace BioscoopReserveringsapplicatie
                 new Option<string>("Film toevoegen", () => AddMovie.Start()),
                 new Option<string>("Filmoverzicht", () => MovieOverview.Start()),
                 new Option<string>("Experience toevoegen", () => AddExperience.Start()),
-                // new Option<string>("Experienceoverzicht"),
+                new Option<string>("Experienceoverzicht", () => ExperienceOverview.Start()),
                 new Option<string>("Uitloggen", () => LandingPage.Start()),
             };
                 SelectionMenu.Create(options, () => Console.WriteLine($"Welkom {CurrentUser.FullName}!\n"));
---
+++ BioscoopReserveringsapplicatie/Presentation/ExperienceDelete.cs
@@ -0,0 +1,10 @@
+namespace BioscoopReserveringsapplicatie
+{
+    static class ExperienceDelete
+    {
+        public static void Start(int experienceId)
+        {
+
+        }
+    }
+}
