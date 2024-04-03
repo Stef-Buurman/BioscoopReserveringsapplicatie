344f69bc03dfc8468a46be687c9564b0883a4d12
Stef Buurman <St3fbuurman@gmail.com>
2024-04-03 14:04:35 +0200

    Changed name of private variable

--- BioscoopReserveringsapplicatie/DataAccess/UserAccess.cs
+++ BioscoopReserveringsapplicatie/DataAccess/UserAccess.cs
@@ -3,10 +3,10 @@ namespace BioscoopReserveringsapplicatie
     public static class UserAccess
     {
         private static readonly string Filename = "User.json";
-        private static IDataAccess<UserModel> DataAccess = new DataAccess<UserModel>(Filename);
-        public static void NewDataAccess(IDataAccess<UserModel> dataAccess) => DataAccess = dataAccess;
-        public static List<UserModel> LoadAll() => DataAccess.LoadAll();
+        private static IDataAccess<UserModel> _dataAccess = new DataAccess<UserModel>(Filename);
+        public static void NewDataAccess(IDataAccess<UserModel> dataAccess) => _dataAccess = dataAccess;
+        public static List<UserModel> LoadAll() => _dataAccess.LoadAll();
 
-        public static void WriteAll(List<UserModel> accounts) => DataAccess.WriteAll(accounts);
+        public static void WriteAll(List<UserModel> accounts) => _dataAccess.WriteAll(accounts);
     }
 }
