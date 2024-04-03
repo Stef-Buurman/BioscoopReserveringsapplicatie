7c02f79382741c63abd7b74ba7c11a12b0432cbe
TvanEert <timvaneert@upcmail.nl>
2024-04-03 14:25:05 +0200

    Created the EditUserdetails From with use of the new enums.

--- BioscoopReserveringsapplicatie/Presentation/UserDetailsEdit.cs
+++ BioscoopReserveringsapplicatie/Presentation/UserDetailsEdit.cs
@@ -4,9 +4,39 @@ namespace BioscoopReserveringsapplicatie
     {
         public static void Start()
         {
-            Console.Clear();
+            if(UserLogic.CurrentUser != null)
+            {
+                Console.Clear();
 
-            Console.WriteLine("Edit user details");
+                Console.WriteLine("Voer nieuwe profielgegevens in (druk op Enter om de huidige te behouden):");
+
+                Console.Write("Voer uw naam in: ");
+                string newName = EditDefaultValueUtil.EditDefaultValue(UserLogic.CurrentUser.FullName);
+
+                Console.Write("Voer uw emailadres in: ");
+                string newEmail = EditDefaultValueUtil.EditDefaultValue(UserLogic.CurrentUser.EmailAddress);
+
+                List<Genre> newGenres = new List<Genre>();
+                List<Genre> availableGenres = Enum.GetValues(typeof(Genre)).Cast<Genre>().ToList();
+
+                while (newGenres.Count < 3)
+                {
+                    Genre newGenre = SelectionMenu.Create(availableGenres, () => ColorConsole.WriteColorLine("Kies een [genre]: \n", Globals.ColorInputcClarification));
+
+                    if(availableGenres.Contains(newGenre))
+                    {
+                        availableGenres.Remove(newGenre);
+                        newGenres.Add(newGenre);
+                    }
+                }
+
+                List<Intensity> intensities = Enum.GetValues(typeof(Intensity)).Cast<Intensity>().ToList();
+                Intensity newIntensity = SelectionMenu.Create(intensities, () => ColorConsole.WriteColorLine("Kies een [Intensiteit]: \n", Globals.ColorInputcClarification));
+
+                List<AgeCategory> ageCategories = Enum.GetValues(typeof(AgeCategory)).Cast<AgeCategory>().ToList();
+                AgeCategory newAgeCategory = SelectionMenu.Create(ageCategories, () => ColorConsole.WriteColorLine("Kies een [Kijkwijzer]: \n", Globals.ColorInputcClarification));
+
+            }
         }
     }
 }
