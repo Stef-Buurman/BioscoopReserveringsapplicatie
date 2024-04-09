2ced01743580b0a6c515078b76b07992f78650a8
Stef Buurman <St3fbuurman@gmail.com>
2024-04-09 16:19:15 +0200

    Created IdGenerator

---
+++ BioscoopReserveringsapplicatie/Logic/IdGenerator.cs
@@ -0,0 +1,17 @@
+﻿namespace BioscoopReserveringsapplicatie
+{
+    public static class IdGenerator
+    {
+        public static int GetNextId<T>(List<T> experiences) where T : IID
+        {
+            if (experiences.Count > 0)
+            {
+                return experiences.Max(exp => exp.Id) + 1;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
