d10c5b3f27e67639174e627de9fc3d18c98be461
NickWijmans <nickwijmans@gmail.com>
2024-04-09 14:31:57 +0200

    Adjusted login and user profile preview

--- BioscoopReserveringsapplicatie/Presentation/Users/UserDetails.cs
+++ BioscoopReserveringsapplicatie/Presentation/Users/UserDetails.cs
@@ -7,23 +7,23 @@ namespace BioscoopReserveringsapplicatie
             List<Option<string>> options = new List<Option<string>>
             {
                 new Option<string>("Profielgegevens bewerken",() => UserDetailsEdit.Start()),
-                new Option<string>("Terug", () => Profile.Start())
+                new Option<string>("Terug", () => UserMenu.Start())
             };
             SelectionMenuUtil.Create(options, () => UserInfo());
         }
 
         private static void UserInfo()
         {
-            if(UserLogic.CurrentUser != null)
+            if (UserLogic.CurrentUser != null)
             {
                 Console.Clear();
                 ColorConsole.WriteColorLine("[Profielgegevens]", ConsoleColor.Cyan);
                 ColorConsole.WriteColorLine($"[Naam: ]{UserLogic.CurrentUser.FullName}", ConsoleColor.Cyan);
                 ColorConsole.WriteColorLine($"[Email: ]{UserLogic.CurrentUser.EmailAddress}\n", ConsoleColor.Cyan);
                 ColorConsole.WriteColorLine("[Persoonlijke voorkeuren]", ConsoleColor.Green);
-                ColorConsole.WriteColorLine($"[Genre: ]{string.Join(", ", UserLogic.CurrentUser.Genres)}", ConsoleColor.Green);
-                ColorConsole.WriteColorLine($"[Intensiteit: ]{UserLogic.CurrentUser.Intensity}", ConsoleColor.Green);
-                ColorConsole.WriteColorLine($"[Kijkwijzer: ]{UserLogic.CurrentUser.AgeCategory.GetDisplayName()}\n", ConsoleColor.Green);
+                ColorConsole.WriteColorLine($"[Genre: ]{(UserLogic.CurrentUser.Genres.Any() ? string.Join(", ", UserLogic.CurrentUser.Genres) : "Undefined")}", ConsoleColor.Green);
+                ColorConsole.WriteColorLine($"[Kijkwijzer: ]{UserLogic.CurrentUser.AgeCategory}", ConsoleColor.Green);
+                ColorConsole.WriteColorLine($"[Intensiteit: ]{UserLogic.CurrentUser.Intensity}\n", ConsoleColor.Green);
             }
         }
     }
