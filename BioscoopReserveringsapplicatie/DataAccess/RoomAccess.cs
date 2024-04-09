19ed8a677577ce4562bbc2c12fe2f8589a1e3ebf
TvanEert <timvaneert@upcmail.nl>
2024-04-09 16:16:35 +0200

    Added the RoomLogic for getting and setting Rooms, Added the Rooms.json to save Room data. Changed naming to be more inline with rest of the code.

--- BioscoopReserveringsapplicatie/DataAccess/RoomAccess.cs
+++ BioscoopReserveringsapplicatie/DataAccess/RoomAccess.cs
@@ -2,11 +2,11 @@ namespace BioscoopReserveringsapplicatie
 {
     static class RoomAccess
     {
-        private static readonly string Filename = "Location.json";
+        private static readonly string Filename = "Rooms.json";
         private static readonly DataAccess<RoomModel> _dataAccess = new DataAccess<RoomModel>(Filename);
 
         public static List<RoomModel> LoadAll() => _dataAccess.LoadAll(); 
 
-        public static void WriteAll(List<RoomModel> locations) => _dataAccess.WriteAll(locations);
+        public static void WriteAll(List<RoomModel> rooms) => _dataAccess.WriteAll(rooms);
     }
 }
