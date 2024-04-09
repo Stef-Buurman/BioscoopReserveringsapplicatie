25bdd75aeba3ba644ff7935c0618b8788c7f7e5e
NickWijmans <nickwijmans@gmail.com>
2024-04-09 15:21:57 +0200

    Finalized feature optional preferences for user

--- BioscoopReserveringsapplicatie/Presentation/Users/UserDetails.cs
+++ BioscoopReserveringsapplicatie/Presentation/Users/UserDetails.cs
@@ -22,7 +22,7 @@ namespace BioscoopReserveringsapplicatie
                 ColorConsole.WriteColorLine($"[Email: ]{UserLogic.CurrentUser.EmailAddress}\n", ConsoleColor.Cyan);
                 ColorConsole.WriteColorLine("[Persoonlijke voorkeuren]", ConsoleColor.Green);
                 ColorConsole.WriteColorLine($"[Genre: ]{(UserLogic.CurrentUser.Genres.Any() ? string.Join(", ", UserLogic.CurrentUser.Genres) : "Undefined")}", ConsoleColor.Green);
-                ColorConsole.WriteColorLine($"[Kijkwijzer: ]{UserLogic.CurrentUser.AgeCategory}", ConsoleColor.Green);
+                ColorConsole.WriteColorLine($"[Kijkwijzer: ]{UserLogic.CurrentUser.AgeCategory.GetDisplayName()}", ConsoleColor.Green);
                 ColorConsole.WriteColorLine($"[Intensiteit: ]{UserLogic.CurrentUser.Intensity}\n", ConsoleColor.Green);
             }
         }
--- BioscoopReserveringsapplicatie/Presentation/Users/UserDetailsEdit.cs
+++ BioscoopReserveringsapplicatie/Presentation/Users/UserDetailsEdit.cs
@@ -81,7 +81,7 @@ namespace BioscoopReserveringsapplicatie
             ColorConsole.WriteColorLine($"[Email: ]{newEmail}\n", ConsoleColor.Cyan);
             ColorConsole.WriteColorLine("[Persoonlijke voorkeuren]", ConsoleColor.Green);
             ColorConsole.WriteColorLine($"[Genre: ]{(newGenres.Any() ? string.Join(", ", newGenres) : "Undefined")}", ConsoleColor.Green);
-            ColorConsole.WriteColorLine($"[Kijkwijzer: ]{newAgeCategory}", ConsoleColor.Green);
+            ColorConsole.WriteColorLine($"[Kijkwijzer: ]{newAgeCategory.GetDisplayName()}", ConsoleColor.Green);
             ColorConsole.WriteColorLine($"[Intensiteit: ]{newIntensity}\n", ConsoleColor.Green);
             ColorConsole.WriteColorLine("[Weet je zeker dat je de gegevens wilt aanpassen ?]", ConsoleColor.Red);
         }
