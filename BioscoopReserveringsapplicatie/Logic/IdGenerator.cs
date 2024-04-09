fee395ef89c9ab13dde2cb7650ac20f4169da8ee
Stef Buurman <St3fbuurman@gmail.com>
2024-04-09 16:30:14 +0200

    Created lambda function instead of normal function

--- BioscoopReserveringsapplicatie/Logic/IdGenerator.cs
+++ BioscoopReserveringsapplicatie/Logic/IdGenerator.cs
@@ -2,16 +2,6 @@
 {
     public static class IdGenerator
     {
-        public static int GetNextId<T>(List<T> items) where T : IID
-        {
-            if (items.Count > 0)
-            {
-                return items.Max(item => item.Id) + 1;
-            }
-            else
-            {
-                return 1;
-            }
-        }
+        public static int GetNextId<T>(List<T> items) where T : IID => items.Count > 0 ? items.Max(item => item.Id) + 1 : 1;
     }
 }
