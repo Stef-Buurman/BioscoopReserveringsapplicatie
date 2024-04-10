d066117c1367b11f81973db839eae2781111d7c5
Stef Buurman <St3fbuurman@gmail.com>
2024-04-10 13:40:05 +0200

    Created function for multiple colors on one Write or WriteLine

--- BioscoopReserveringsapplicatie/Presentation/Utilities/ColorConsole.cs
+++ BioscoopReserveringsapplicatie/Presentation/Utilities/ColorConsole.cs
@@ -9,23 +9,64 @@ namespace BioscoopReserveringsapplicatie
             WriteColor(message, color);
             Console.WriteLine();
         }
+
+        public static void WriteColorLine(string message)
+        {
+            WriteColor(message);
+            Console.WriteLine();
+        }
         public static void WriteColor(string message, ConsoleColor color)
         {
             string[] piecesOfMessage = Regex.Split(message, @"(\[[^\]]*\])");
 
-            for (int i = 0; i < piecesOfMessage.Length; i++)
-            {
-                string messagePiece = piecesOfMessage[i];
+        static void WriteColor(string message)
+        {
+            ConsoleColor originalForeColor = Console.ForegroundColor;
 
-                if (messagePiece.StartsWith("[") && messagePiece.EndsWith("]"))
-                {
-                    Console.ForegroundColor = color;
-                    messagePiece = messagePiece.Substring(1, messagePiece.Length - 2);
+            int startIndex = 0;
+            int openBracketIndex = message.IndexOf('[');
+
+            while (openBracketIndex != -1)
+            {
+                int closeBracketIndex = message.IndexOf(']', openBracketIndex);
+                if (closeBracketIndex == -1)
+            {
+                    Console.ForegroundColor = originalForeColor;
+                    Console.WriteLine(message.Substring(startIndex));
+                    return;
                 }
 
-                Console.Write(messagePiece);
-                Console.ResetColor();
+                Console.ForegroundColor = originalForeColor;
+                Console.Write(message.Substring(startIndex, openBracketIndex - startIndex));
+
+                string colorString = message.Substring(openBracketIndex + 1, closeBracketIndex - openBracketIndex - 1);
+                ConsoleColor color;
+                if (Enum.TryParse(colorString, true, out color))
+                {
+                    int endTagIndex = message.IndexOf("[/]", closeBracketIndex);
+                    if (endTagIndex == -1)
+                {
+                        Console.ForegroundColor = originalForeColor;
+                        Console.WriteLine(message.Substring(startIndex));
+                        return;
+                }
+
+                    WriteColor(message.Substring(closeBracketIndex + 1, endTagIndex - closeBracketIndex - 1), color);
+
+                    startIndex = endTagIndex + 3;
+
+                    openBracketIndex = message.IndexOf('[', startIndex);
+                }
+                else
+                {
+                    Console.Write($"[{colorString}]");
+                    startIndex = closeBracketIndex + 1;
+                    openBracketIndex = message.IndexOf('[', startIndex);
+                }
             }
+
+            Console.ForegroundColor = originalForeColor;
+            Console.Write(message.Substring(startIndex));
         }
 
         public static void WriteLineInfo(string message) => WriteColorLine($"[{message}]", ConsoleColor.DarkGray);
