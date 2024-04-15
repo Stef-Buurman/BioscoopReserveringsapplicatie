91da834bc737deda03c8b13f450b88c63b7d1055
Stef Buurman <St3fbuurman@gmail.com>
2024-04-15 16:19:55 +0200

    Used SelectionMenuUtil2 inside the ExperienceDetails

--- BioscoopReserveringsapplicatie/Program.cs
+++ BioscoopReserveringsapplicatie/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace BioscoopReserveringsapplicatie
+﻿using System.Text;
+
+namespace BioscoopReserveringsapplicatie
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
             LandingPage.Start();
         }
     }
