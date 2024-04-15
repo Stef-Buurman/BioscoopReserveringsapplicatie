3156cfdff12f70eac9c05d06c7403a610da862bd
NickWijmans <nickwijmans@gmail.com>
2024-04-15 14:09:41 +0200

    Added PromotionLogic

---
+++ BioscoopReserveringsapplicatie/Logic/PromotionLogic.cs
@@ -0,0 +1,77 @@
+namespace BioscoopReserveringsapplicatie
+{
+    public class PromotionLogic
+    {
+        private List<PromotionModel> _promotions = new();
+
+        public PromotionLogic()
+        {
+            _promotions = PromotionAccess.LoadAll();
+        }
+
+        public List<PromotionModel> GetAll()
+        {
+            _promotions = PromotionAccess.LoadAll();
+            return _promotions;
+        }
+
+        public PromotionModel? GetById(int id)
+        {
+            _promotions = PromotionAccess.LoadAll();
+            return _promotions.Find(s => s.Id == id);
+        }
+
+        public bool Add(string title, string description)
+        {
+            if (ValidatePromotionTitle(title) && ValidatePromotionDescription(description))
+            {
+                PromotionModel promotion = new PromotionModel(IdGenerator.GetNextId(_promotions), title, description, false);
+
+                if (this.ValidatePromotion(promotion))
+                {
+                    UpdateList(promotion);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool ValidatePromotion(PromotionModel promotion)
+        {
+            if (promotion == null) return false;
+            else if (!ValidatePromotionTitle(promotion.Title)) return false;
+            else if (!ValidatePromotionDescription(promotion.Description)) return false;
+            return true;
+        }
+
+        public bool ValidatePromotionTitle(string title)
+        {
+            return !string.IsNullOrWhiteSpace(title);
+        }
+
+        public bool ValidatePromotionDescription(string description)
+        {
+            return !string.IsNullOrWhiteSpace(description);
+        }
+
+        public void UpdateList(PromotionModel promotion)
+        {
+            //Find if there is already an model with the same id
+            int index = _promotions.FindIndex(s => s.Id == promotion.Id);
+
+            if (index != -1)
+            {
+                //update existing model
+                _promotions[index] = promotion;
+            }
+            else
+            {
+                //add new model
+                _promotions.Add(promotion);
+            }
+            PromotionAccess.WriteAll(_promotions);
+            _promotions = PromotionAccess.LoadAll();
+        }
+    }
+}
