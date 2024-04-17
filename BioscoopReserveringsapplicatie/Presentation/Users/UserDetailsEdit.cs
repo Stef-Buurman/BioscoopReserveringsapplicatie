7a54d9cbfb3c05daba2b381960905929c5dc8379
TvanEert <timvaneert@upcmail.nl>
2024-04-17 11:00:40 +0200

    W.I.P UserDetailsEdit presentation logic

--- BioscoopReserveringsapplicatie/Presentation/Users/UserDetailsEdit.cs
+++ BioscoopReserveringsapplicatie/Presentation/Users/UserDetailsEdit.cs
@@ -6,53 +6,41 @@ namespace BioscoopReserveringsapplicatie
     {
         private static UserLogic _userLogic = new UserLogic();
         private static Action actionWhenEscapePressed = UserDetails.Start;
-        public static void Start()
+
+        private static string newName = "";
+        private static string newEmail = "";
+        private static List<Genre> newGenres = new List<Genre>();
+        private static AgeCategory newAgeCategory = AgeCategory.Undefined;
+        private static Intensity newintensity = Intensity.Undefined;
+
+        private static string _returnToName = "Name";
+        private static string _returnToEmail = "Email";
+        private static string _returnToGenres = "Genres";
+        private static string _returnToRating = "Rating";
+        private static string _returnToIntensity = "Intensity";
+
+        public static void Start(string returnTo = "")
         {
             if (UserLogic.CurrentUser != null)
             {
-                string newName = "";
-                bool validName = false;
-                while (!validName)
-                {
-                    Console.Clear();
-                    newName = ReadLineUtil.EditValue(UserLogic.CurrentUser.FullName,
-                        () =>
-                        {
-                            ColorConsole.WriteColor("Voer uw [naam] in: ", Globals.ColorInputcClarification);
-                        },
-                        actionWhenEscapePressed,
-                        "(druk op Enter om de huidige waarde te behouden en op Esc om terug te gaan)\n"
-                    );
-                    validName = _userLogic.ValidateName(newName);
-                }
+                if (returnTo == "" || returnTo == _returnToName) UserName();
+                if (returnTo == "" || returnTo == _returnToEmail) UserEmail();
+                if (returnTo == "" || returnTo == _returnToGenres) UserGenres();
+                if (returnTo == "" || returnTo == _returnToRating)  UserRating();
+                if (returnTo == "" || returnTo == _returnToIntensity) UserIntensity();
 
-                string newEmail = "";
-                bool validEmail = false;
-                while (!validEmail)
-                {
-                    Console.Clear();
-                    newEmail = ReadLineUtil.EditValue(UserLogic.CurrentUser.EmailAddress,
-                        () =>
-                        {
-                            ColorConsole.WriteColorLine($"Voer uw [naam] in: {newName}", Globals.ColorInputcClarification);
-                            ColorConsole.WriteColor("Voer uw [emailadres] in: ", Globals.ColorInputcClarification);
-                        },
-                        actionWhenEscapePressed,
-                        "(druk op Enter om de huidige waarde te behouden en op Esc om terug te gaan)\n"
-                    );
-                    validEmail = _userLogic.ValidateEmail(newEmail);
-                }
+                
 
-                List<Genre> selectedGenres = Preferences.SelectGenres();
+                newGenres = Preferences.SelectGenres();
 
-                AgeCategory ageCategory = Preferences.SelectAgeCategory();
+                newAgeCategory = Preferences.SelectAgeCategory();
 
-                Intensity intensity = Preferences.SelectIntensity();
+                newintensity = Preferences.SelectIntensity();
 
                 List<Option<string>> options = new List<Option<string>>
                 {
                     new Option<string>("Ja", () => {
-                        if(_userLogic.Edit(UserLogic.CurrentUser.Id, newName, newEmail, selectedGenres, intensity, ageCategory))
+                        if(_userLogic.Edit(UserLogic.CurrentUser.Id, newName, newEmail, newGenres, newintensity, newAgeCategory))
                         {
                             Console.Clear();
                             ColorConsole.WriteColorLine("Gebruikersgegevens zijn gewijzigd!", Globals.SuccessColor);
@@ -70,8 +58,62 @@ namespace BioscoopReserveringsapplicatie
                     }),
                     new Option<string>("Nee", actionWhenEscapePressed)
                 };
-                SelectionMenuUtil.Create(options, () => PendingChanges(newName, newEmail, selectedGenres, intensity, ageCategory));
+                SelectionMenuUtil.Create(options, () => PendingChanges(newName, newEmail, newGenres, newintensity, newAgeCategory));
             }
+        }
+
+        private static void UserName()
+        {
+            newName = "";
+            bool validName = false;
+            while (!validName)
+            {
+                Console.Clear();
+                newName = ReadLineUtil.EditValue(UserLogic.CurrentUser.FullName,
+                    () =>
+                    {
+                        ColorConsole.WriteColor("Voer uw [naam] in: ", Globals.ColorInputcClarification);
+                    },
+                    actionWhenEscapePressed,
+                    "(druk op Enter om de huidige waarde te behouden en op Esc om terug te gaan)\n"
+                );
+                validName = _userLogic.ValidateName(newName);
+            }
+        }
+
+        private static void UserEmail()
+        {
+            newEmail = "";
+            bool validEmail = false;
+            while (!validEmail)
+            {
+                Console.Clear();
+                newEmail = ReadLineUtil.EditValue(UserLogic.CurrentUser.EmailAddress,
+                    () =>
+                    {
+                        ColorConsole.WriteColorLine($"Voer uw [naam] in: {newName}", Globals.ColorInputcClarification);
+                        ColorConsole.WriteColor("Voer uw [emailadres] in: ", Globals.ColorInputcClarification);
+                    },
+                    () => Start(_returnToName),
+                    "(druk op Enter om de huidige waarde te behouden en op Esc om terug te gaan)\n"
+                );
+                validEmail = _userLogic.ValidateEmail(newEmail);
+            }
+        }
+
+        private static void UserGenres()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void UserRating()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void UserIntensity()
+        {
+            throw new NotImplementedException();
         }
 
         private static void PendingChanges(string newName, string newEmail, List<Genre> newGenres, Intensity newIntensity, AgeCategory newAgeCategory)
