eb97d0f3b81188652f7b9679673027e92bd2c9b8
Pieter Smit <1058663@hr.nl>
2024-04-17 12:15:54 +0200

    Added right padding between data + added "..." after 25 characters

--- BioscoopReserveringsapplicatie/Presentation/Experiences/ExperienceOverview.cs
+++ BioscoopReserveringsapplicatie/Presentation/Experiences/ExperienceOverview.cs
@@ -114,12 +114,6 @@ namespace BioscoopReserveringsapplicatie
             Start();
         }
 
-        // private static void Print()
-        // {
-        //     Console.WriteLine("Dit zijn alle experiences die momenteel beschikbaar zijn:\n");
-        //     Console.WriteLine("Experience Naam   Genres                 Leeftijdscategorie  Intensiteit  Gearchiveerd");
-        //     Console.WriteLine("----------------  ---------------------  ------------------  -----------  ------------");
-        // }
         private static void Print()
         {
             // Defineer de kolom koppen voor de tabel
