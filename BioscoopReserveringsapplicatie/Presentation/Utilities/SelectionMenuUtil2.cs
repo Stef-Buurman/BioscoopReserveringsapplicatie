1d8c3766a28c032f85344c0b2a31da5d97795bcc
TvanEert <timvaneert@upcmail.nl>
2024-04-17 17:04:39 +0200

    Remane in ScheduleExperience and W.I.P CustomKeyAction SelectionMenuUtil

--- BioscoopReserveringsapplicatie/Presentation/Utilities/SelectionMenuUtil2.cs
+++ BioscoopReserveringsapplicatie/Presentation/Utilities/SelectionMenuUtil2.cs
@@ -278,6 +278,8 @@
                     //() => WriteMenu(GetOptionsToShow(Options, MaxVisibility, AmountOptionsAbove, (AmountOptionsAbove > 0))
                     ReadLineUtil.EscapeKeyPressed(() => { }, EscapeAction, EscapeActionWhenNotEscaping);
                 }
+
+
             }
             while (keyinfo.Key != ConsoleKey.X);
             Console.CursorVisible = true;
