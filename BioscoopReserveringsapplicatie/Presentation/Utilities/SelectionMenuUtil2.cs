bd11ed994aff8483bc9eea1b492d30e1bb4f6c0a
Stef Buurman <St3fbuurman@gmail.com>
2024-04-18 13:44:25 +0200

    Created new constructor and new check for when no selectedOption is send

--- BioscoopReserveringsapplicatie/Presentation/Utilities/SelectionMenuUtil2.cs
+++ BioscoopReserveringsapplicatie/Presentation/Utilities/SelectionMenuUtil2.cs
@@ -61,7 +61,10 @@
                 EscapeAction = escapeAction;
                 EscapeActionWhenNotEscaping = escapeActionWhenNotEscaping;
             }
-            SelectedOption = selectedOption;
+            if (selectedOption != default && AllOptions.Contains(selectedOption))
+            {
+                SelectedOption = selectedOption;
+            }
             CanBeEscaped = canBeEscaped;
             EscapeAction = escapeAction;
             EscapeActionWhenNotEscaping = escapeActionWhenNotEscaping;
@@ -90,6 +93,9 @@
             : this(options, maxVisibility, true, escapeAction, escapeActionWhenNotEscaping, true, default, selectedOption) { }
 
         public SelectionMenuUtil2(List<Option<T>> options, Action escapeAction, Action escapeActionWhenNotEscaping, Option<T> selectedOption)
+            : this(options, 9, true, escapeAction, escapeActionWhenNotEscaping, true, default, selectedOption) { }
+
+        public SelectionMenuUtil2(List<T> options, Action escapeAction, Action escapeActionWhenNotEscaping, Option<T> selectedOption)
             : this(options, 9, true, escapeAction, escapeActionWhenNotEscaping, true, default, selectedOption) { }
 
         public SelectionMenuUtil2(List<Option<T>> options)
@@ -133,6 +139,8 @@
         public SelectionMenuUtil2(List<T> options, int maxVisibility, Action escapeAction, Action escapeActionWhenNotEscaping, bool visibleSelectedArrows = true, string textBeforeInputShown = default) 
             : this(options, maxVisibility, true, escapeAction, escapeActionWhenNotEscaping, visibleSelectedArrows, textBeforeInputShown) { }
 
+        public SelectionMenuUtil2(List<T> options, int maxVisibility, Action escapeAction, Action escapeActionWhenNotEscaping, bool visibleSelectedArrows = true, string textBeforeInputShown = default, Option<T> selectedOption = default)
+            : this(options, maxVisibility, true, escapeAction, escapeActionWhenNotEscaping, visibleSelectedArrows, textBeforeInputShown, selectedOption) { }
 
         public T Create()
         {
