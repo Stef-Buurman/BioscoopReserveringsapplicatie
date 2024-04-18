15893e443e91786dde9b750b406f4234bde951d6
Tim van Eert <99051246@mydavinci.nl>
2024-04-18 15:58:11 +0200

    WIP KeyAction For SelectionMenuUtil and rename ScheduleExperince to ScheduleExperience

--- BioscoopReserveringsapplicatie/Presentation/Utilities/SelectionMenuUtil2.cs
+++ BioscoopReserveringsapplicatie/Presentation/Utilities/SelectionMenuUtil2.cs
@@ -3,7 +3,6 @@
     public class SelectionMenuUtil2<T>
     {
         private static int MaxSelectionMenu = 0;
-
         private int Index = 0;
         private int VisibleIndex = 0;
 
@@ -86,7 +85,7 @@
         public SelectionMenuUtil2(List<Option<T>> options, Action escapeAction, Action escapeActionWhenNotEscaping, bool visibleSelectedArrows = true, string textBeforeInputShown = default)
             : this(options, 9, true, escapeAction, escapeActionWhenNotEscaping, visibleSelectedArrows, textBeforeInputShown) { }
 
-        public SelectionMenuUtil2(List<Option<T>> options, int maxVisibility, Action escapeAction, Action escapeActionWhenNotEscaping, bool visibleSelectedArrows = true, string textBeforeInputShown = default) 
+        public SelectionMenuUtil2(List<Option<T>> options, int maxVisibility, Action escapeAction, Action escapeActionWhenNotEscaping, bool visibleSelectedArrows = true, string textBeforeInputShown = default, bool hasCustomKeyAction = false, List<KeyAction> customKeyActions = null) 
             : this(options, maxVisibility, true, escapeAction, escapeActionWhenNotEscaping, visibleSelectedArrows, textBeforeInputShown) { }
 
         public SelectionMenuUtil2(List<Option<T>> options, int maxVisibility, Action escapeAction, Action escapeActionWhenNotEscaping, Option<T> selectedOption)
@@ -187,16 +186,19 @@
                 // When the user presses the down arrow, the selected option will move down
                 if (keyinfo.Key == ConsoleKey.DownArrow)
                 {
+                    KeysInUse.Add(ConsoleKey.DownArrow);
                     KeyDown();
                 }
                 // When the user presses the up arrow, this will be executed.
                 if (keyinfo.Key == ConsoleKey.UpArrow)
                 {
+                    KeysInUse.Add(ConsoleKey.UpArrow);
                     KeyUp();
                 }
                 // When the user presses the enter key, the selected option will be executed
                 if (keyinfo.Key == ConsoleKey.Enter)
                 {
+                    KeysInUse.Add(ConsoleKey.Enter);
                     Console.CursorVisible = true;
                     AllOptions[Index].Select();
                     return AllOptions[Index].Value;
@@ -204,6 +206,7 @@
 
                 if (keyinfo.Key == ConsoleKey.Escape && CanBeEscaped && EscapeAction != null)
                 {
+                    KeysInUse.Add(ConsoleKey.Escape);
                     //() => WriteMenu(GetOptionsToShow(Options, MaxVisibility, AmountOptionsAbove, (AmountOptionsAbove > 0))
                     ReadLineUtil.EscapeKeyPressed(() => { }, EscapeAction, EscapeActionWhenNotEscaping);
                 }
