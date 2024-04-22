699fcb20dc2cabea2a916322f0fa4b6acc7af196
Stef Buurman <St3fbuurman@gmail.com>
2024-04-22 14:09:35 +0200

    Added new parameter and isSelected

--- BioscoopReserveringsapplicatie/DataModels/Option.cs
+++ BioscoopReserveringsapplicatie/DataModels/Option.cs
@@ -7,23 +7,25 @@
         public Action Selected { get; }
         public bool IsSelected { get; private set; }
 
-        public Option(T value, string name, Action selected) : this(value, name)
+        public Option(T value, string name, Action selected, bool isSelected = false) : this(value, name, isSelected)
         {
             Selected = selected;
         }
-        public Option(T value, string name)
+        public Option(T value, string name, bool isSelected = false)
         {
             Name = name;
             Value = value;
+            IsSelected = isSelected;
         }
-        public Option(T name, Action selected) : this(name)
+        public Option(T name, Action selected, bool isSelected = false) : this(name, isSelected)
         {
             Selected = selected;
         }
-        public Option(T name)
+        public Option(T name, bool isSelected = false)
         {
             Name = name.ToString();
             Value = name;
+            IsSelected = isSelected;
         }
 
         public void InvertSelecttion() => IsSelected = !IsSelected;
