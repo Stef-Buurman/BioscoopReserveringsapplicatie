f9e315b8edafe77d41ee624e164c67d715647b95
NickWijmans <nickwijmans@gmail.com>
2024-04-22 14:24:53 +0200

    Started implementing saving reservations

---
+++ BioscoopReserveringsapplicatie/Logic/ReservationLogic.cs
@@ -0,0 +1,77 @@
+namespace BioscoopReserveringsapplicatie
+{
+    public class ReservationLogic
+    {
+        private List<ReservationModel> _reservations = new();
+
+        public ReservationLogic()
+        {
+            _reservations = ReservationAccess.LoadAll();
+        }
+
+        public List<ReservationModel> GetAll()
+        {
+            _reservations = ReservationAccess.LoadAll();
+            return _reservations;
+        }
+
+        public ReservationModel? GetById(int id)
+        {
+            _reservations = ReservationAccess.LoadAll();
+            return _reservations.Find(s => s.Id == id);
+        }
+
+        public bool Complete(int experienceId, int? locationId, DateTime? datetime, int? roomId, int userId)
+        {
+            GetAll();
+
+            if (experienceId != 0 && locationId != null && datetime != null && roomId != null)
+            {
+                ReservationModel reservation = new ReservationModel(IdGenerator.GetNextId(_reservations), experienceId, locationId, datetime, roomId, userId);
+
+                if (this.Validate(reservation))
+                {
+                    UpdateList(reservation);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool Validate(ReservationModel reservation)
+        {
+            if (reservation == null) return false;
+
+            return true;
+        }
+
+        // public bool ValidateTitle(string title)
+        // {
+        //     return !string.IsNullOrWhiteSpace(title);
+        // }
+
+        // public bool ValidateDescription(string description)
+        // {
+        //     return !string.IsNullOrWhiteSpace(description);
+        // }
+
+        public void UpdateList(ReservationModel reservation)
+        {
+            //Find if there is already an model with the same id
+            int index = _reservations.FindIndex(s => s.Id == reservation.Id);
+
+            if (index != -1)
+            {
+                //update existing model
+                _reservations[index] = reservation;
+            }
+            else
+            {
+                //add new model
+                _reservations.Add(reservation);
+            }
+            ReservationAccess.WriteAll(_reservations);
+        }
+    }
+}
