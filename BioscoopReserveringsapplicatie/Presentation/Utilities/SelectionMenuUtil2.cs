f9c8736027eeeb702ae1c383930e429cf5870c29
Stef Buurman <St3fbuurman@gmail.com>
2024-04-22 16:05:03 +0200

    Removed commented while loop

--- BioscoopReserveringsapplicatie/Presentation/Utilities/SelectionMenuUtil2.cs
+++ BioscoopReserveringsapplicatie/Presentation/Utilities/SelectionMenuUtil2.cs
@@ -367,7 +367,6 @@
                     strintToPrintForArrowDown = "   ";
                 }
 
-                //while (strintToPrintForArrowDown.Length < TextBeforeInputShown.Length - (TextBeforeInputShownVisible ? 2 : 0)) strintToPrintForArrowDown += " ";
                 // To override the text shown, there must be enough spaces to override the text.
                 while (strintToPrintForArrowDown.Length < MaxSelectionMenu + 3) strintToPrintForArrowDown += " ";
                 Console.WriteLine(strintToPrintForArrowDown);
@@ -400,7 +399,6 @@
                 {
                     strintToPrintForArrowDown = "   ";
                 }
-                //while (strintToPrintForArrowDown.Length < TextBeforeInputShown.Length - (TextBeforeInputShownVisible ? 2 : 0)) strintToPrintForArrowDown += " ";
                 // To override the text shown, there must be enough spaces to override the text.
                 while (strintToPrintForArrowDown.Length < MaxSelectionMenu + 3) strintToPrintForArrowDown += " ";
                 Console.WriteLine(strintToPrintForArrowDown);
