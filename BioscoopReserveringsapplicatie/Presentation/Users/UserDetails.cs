38eb28de8b7b8f3c94dd5b30dfcdf7054cb93f53
NickWijmans <nickwijmans@gmail.com>
2024-05-08 13:36:02 +0200

    Made improvements to user detail and reservations

--- BioscoopReserveringsapplicatie/Presentation/Users/UserDetails.cs
+++ BioscoopReserveringsapplicatie/Presentation/Users/UserDetails.cs
@@ -16,11 +16,15 @@ namespace BioscoopReserveringsapplicatie
             {
                 ColorConsole.WriteColorLine("Profielgegevens", ConsoleColor.Cyan);
                 ColorConsole.WriteColorLine($"[Naam: ]{UserLogic.CurrentUser.FullName}", ConsoleColor.Cyan);
-                ColorConsole.WriteColorLine($"[Email: ]{UserLogic.CurrentUser.EmailAddress}\n", ConsoleColor.Cyan);
+                ColorConsole.WriteColorLine($"[Email: ]{UserLogic.CurrentUser.EmailAddress}", ConsoleColor.Cyan);
+                ColorConsole.WriteColorLine($"[Wachtwoord: ]{new string('*', UserLogic.CurrentUser.Password.Length)}\n", ConsoleColor.Cyan);
+
                 ColorConsole.WriteColorLine("Persoonlijke voorkeuren", ConsoleColor.Green);
                 ColorConsole.WriteColorLine($"[Genre: ]{(UserLogic.CurrentUser.Genres.Any() ? string.Join(", ", UserLogic.CurrentUser.Genres) : "Undefined")}", ConsoleColor.Green);
                 ColorConsole.WriteColorLine($"[Kijkwijzer: ]{UserLogic.CurrentUser.AgeCategory.GetDisplayName()}", ConsoleColor.Green);
-                ColorConsole.WriteColorLine($"[Intensiteit: ]{UserLogic.CurrentUser.Intensity}\n", ConsoleColor.Green);
+                ColorConsole.WriteColorLine($"[Intensiteit: ]{UserLogic.CurrentUser.Intensity.GetDisplayName()}", ConsoleColor.Green);
+                ColorConsole.WriteColorLine($"[Taal: ]{UserLogic.CurrentUser.Language}\n", ConsoleColor.Green);
+
             }
             new SelectionMenuUtil2<string>(options).Create();
         }
