6f5d0048974fa5c57cf587866d7df5728c1161c4
Pieter Smit <1058663@hr.nl>
2024-05-08 11:03:49 +0200

    added ShowPromotion class

--- BioscoopReserveringsapplicatie/Logic/DataLogic/PromotionLogic.cs
+++ BioscoopReserveringsapplicatie/Logic/DataLogic/PromotionLogic.cs
@@ -144,5 +144,10 @@ namespace BioscoopReserveringsapplicatie
             }
             _DataAccess.WriteAll(_promotions);
         }
+        public PromotionModel? GetActivePromotion()
+        {
+            _promotions = _DataAccess.LoadAll();
+            return _promotions.Find(s => s.Status == true);
+        }
     }
 }
