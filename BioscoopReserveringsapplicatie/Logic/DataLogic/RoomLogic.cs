bfe7b8fcd584bbdd60489f7653a79d7000891142
Pieter Smit <1058663@hr.nl>
2024-05-22 10:55:49 +0200

    Archive en Unarchive function in RoomLogic

--- BioscoopReserveringsapplicatie/Logic/DataLogic/RoomLogic.cs
+++ BioscoopReserveringsapplicatie/Logic/DataLogic/RoomLogic.cs
@@ -64,5 +64,33 @@ namespace BioscoopReserveringsapplicatie
             }
             _DataAccess.WriteAll(_Rooms);
         }
+
+        public void Archive(int id)
+        {
+            RoomModel room = GetById(id);
+            if (room != null)
+            {
+                room.Status = Status.Archived;
+                _DataAccess.WriteAll(_Rooms);
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        public void Unarchive(int id)
+        {
+            RoomModel room = GetById(id);
+            if (room != null)
+            {
+                room.Status = Status.Active;
+                _DataAccess.WriteAll(_Rooms);
+            }
+            else
+            {
+                return;
+            }
+        }
     }
 }
