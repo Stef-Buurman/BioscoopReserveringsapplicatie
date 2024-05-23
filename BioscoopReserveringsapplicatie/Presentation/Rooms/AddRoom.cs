1978110f1f757347ef8159c8e8b2f89818bd2663
Pieter Smit <1058663@hr.nl>
2024-05-23 14:55:57 +0200

    finished AddRoom.cs

--- BioscoopReserveringsapplicatie/Presentation/Rooms/AddRoom.cs
+++ BioscoopReserveringsapplicatie/Presentation/Rooms/AddRoom.cs
@@ -3,14 +3,12 @@ namespace BioscoopReserveringsapplicatie
     public static class AddRoom
     {
         private static RoomLogic roomLogic = new RoomLogic();
-        private static LocationLogic locationLogic = new LocationLogic();
-        private static int roomNumber = 0;
-        private static int capacity = 0;
+        private static string roomNumber = "";
+        private static string capacity = "";
 
         public static void Start(int locationId, string returnTo = "")
         {
             Console.Clear();
-            
 
             if (returnTo == "" || returnTo == "RoomNumber")
             {
@@ -19,11 +17,11 @@ namespace BioscoopReserveringsapplicatie
             }
             if (returnTo == "" || returnTo == "Capacity")
             {
-                roomNumber = AskForCapacity();
+                capacity = AskForCapacity();
                 returnTo = "";
             }
-            Print(roomNumber.ToString(), capacity.ToString());
-            RoomModel newRoom = new RoomModel(roomLogic.GetNextId(), locationId, roomNumber, capacity, Status.Active);
+            Print(roomNumber, capacity);
+            RoomModel newRoom = new RoomModel(roomLogic.GetNextId(), locationId, Convert.ToInt32(roomNumber), Convert.ToInt32(capacity), Status.Active);
             List<Option<string>> options = new List<Option<string>>
             {
             new Option<string>("Opslaan en verlaten", () => 
@@ -39,25 +37,22 @@ namespace BioscoopReserveringsapplicatie
                     Start(locationId, "RoomNumber");
                 }
             }),
-            new Option<string>("Verder gaan met aanpassen", () => { Start(locationId, "RoomNumber"); }),
+            new Option<string>("Verder gaan met aanpassen", () => { Start(locationId, "Capacity"); }),
             new Option<string>("Verlaten zonder op te slaan", () => { LocationOverview.Start(); }),
             };
 
             new SelectionMenuUtil<string>(options).Create();
         }
 
-        private static int AskForRoomNumber()
+        private static string AskForRoomNumber()
         {
-            string input = ReadLineUtil.EnterValue("Vul het [Zaalnummer] van de zaal in: ", RoomOverview.Start);
-            int output = Convert.ToInt32(input);
-            return output;
+            ColorConsole.WriteColorLine("Zaal toevoegen\n", Globals.TitleColor);
+            return ReadLineUtil.EnterValue("Vul het [Zaalnummer] van de zaal in: ", RoomOverview.Start);
         }
 
-        private static int AskForCapacity()
+        private static string AskForCapacity()
         {
-            string input = ReadLineUtil.EnterValue("Vul de [Capaciteit] van de zaal in: ", () => Start(0, "RoomNumber"), false, false);
-            int output = Convert.ToInt32(input);
-            return output;
+            return ReadLineUtil.EnterValue("Vul het [Capaciteit] van de zaal in: ", () => Start(0, "RoomNumber"), false, false);
         }
 
         private static void Print(string roomNumber, string capacity)
@@ -67,7 +62,7 @@ namespace BioscoopReserveringsapplicatie
             ColorConsole.WriteColorLine($"[Zaalnummer: ]{roomNumber}", Globals.RoomColor);
             ColorConsole.WriteColorLine($"[Zaal capaciteit: ]{capacity}", Globals.RoomColor);
             HorizontalLine.Print();
-            ColorConsole.WriteColorLine($"Wilt u deze [Zaal] toevoegen?", Globals.ColorInputcClarification);
+            ColorConsole.WriteColorLine($"Wilt u deze [Zaal] toevoegen?\n", Globals.ColorInputcClarification);
         }
     }
 }
