290eaf790df6a3515b1aaf584c1126b29e77f4f9
TvanEert <timvaneert@upcmail.nl>
2024-05-27 14:07:02 +0200

    Added color highlighting to all Custom keyActions

--- BioscoopReserveringsapplicatie/Presentation/Promotions/PromotionOverview.cs
+++ BioscoopReserveringsapplicatie/Presentation/Promotions/PromotionOverview.cs
@@ -47,8 +47,8 @@ namespace BioscoopReserveringsapplicatie
                 string promotionInfo = string.Format("{0,-" + (columnWidths[0] + 2) + "} {1,-" + (columnWidths[1] + 2) + "} {2,-" + (columnWidths[2] + 2) + "}", promotionTitle, promotionDescription, promotion.Status.GetDisplayName());
                 options.Add(new Option<int>(promotion.Id, promotionInfo));
             }
-            ColorConsole.WriteLineInfo("*Klik op escape om dit onderdeel te verlaten*\n");
-            ColorConsole.WriteLineInfo("Klik op T om een promotie toe te voegen.\n");
+            ColorConsole.WriteLineInfoHighlight("*Klik op [escape] om dit terug te gaan*", Globals.ColorInputcClarification);
+            ColorConsole.WriteLineInfoHighlight("*Klik op [T] om een promotie toe te voegen*\n", Globals.ColorInputcClarification);
             ColorConsole.WriteColorLine("Dit zijn alle promoties die momenteel bestaan:\n", Globals.TitleColor);
             Print();
             int promotionId = new SelectionMenuUtil<int>(options,
