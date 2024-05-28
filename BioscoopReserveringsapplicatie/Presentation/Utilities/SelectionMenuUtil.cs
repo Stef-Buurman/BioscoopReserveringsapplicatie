c1cb7371c82d16d01d947b15b34d74c9c18b280c
Stef Buurman <St3fbuurman@gmail.com>
2024-05-28 14:35:10 +0200

    Console.SetCursorPosition(0, top); test

--- BioscoopReserveringsapplicatie/Presentation/Utilities/SelectionMenuUtil.cs
+++ BioscoopReserveringsapplicatie/Presentation/Utilities/SelectionMenuUtil.cs
@@ -628,6 +628,7 @@ namespace BioscoopReserveringsapplicatie
                 while (stringtToPrint.Length < MaxSelectionMenu) stringtToPrint += " ";
                 ColorConsole.WriteColorLine(stringtToPrint);
             }
+            Console.SetCursorPosition(0, Top);
         }
 
         private void Move(int rowDelta, int colDelta)
