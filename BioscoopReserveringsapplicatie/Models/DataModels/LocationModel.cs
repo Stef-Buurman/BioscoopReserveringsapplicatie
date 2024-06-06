8d86bfdc34e1f6416d35beb64cbeabeccf02fd8e
Pieter Smit <1058663@hr.nl>
2024-06-06 13:32:59 +0200

    changed LocationModel to make Unarchive tests actually test Unarchive

--- BioscoopReserveringsapplicatie/Models/DataModels/LocationModel.cs
+++ BioscoopReserveringsapplicatie/Models/DataModels/LocationModel.cs
@@ -20,12 +20,5 @@ namespace BioscoopReserveringsapplicatie
             Name = name;
             Status = status;
         }
-
-        public LocationModel(int id, string name, Status status)
-        {
-            Id = id;
-            Name = name;
-            Status = status;
-        }
     }
 }
